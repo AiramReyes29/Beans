5f78ad9c91733d85ed3690473e7ef25648a2cf41
AiramReyes29 <90471939+AiramReyes29@users.noreply.github.com>
2021-11-09 13:09:12 -0600

    Add files via upload

---
+++ everis-trainee/spring-core/src/main/java/com/example/springcoredojo/NTT/Prueba.java
@@ -0,0 +1,17 @@
+package com.example.springcoredojo.NTT;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+    @Configuration
+    @ComponentScan(basePackageClasses = Alumno.class)
+    public class Prueba {
+        @Bean
+        public Alumno getAddress() {
+            return new Alumno("Violet", "Martinez Reyes",23);
+        }
+    }
+
+
